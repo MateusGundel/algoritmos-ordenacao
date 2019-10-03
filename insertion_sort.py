00f111fa2884a9d9a5925139d307511fee16bcee
Mateus Gundel <mateus8923@gmail.com>
2019-10-02 22:12:38 -0300

    [FIX] corrigido retorno do insertion

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,15 +1,17 @@
-def insertionSort(arr): 
+def insertionSort(items): 
   
     # Traverse through 1 to len(arr) 
-    for i in range(1, len(arr)): 
+    for i in range(1, len(items)): 
   
-        key = arr[i] 
+        key = items[i] 
   
         # Move elements of arr[0..i-1], that are 
         # greater than key, to one position ahead 
         # of their current position 
         j = i-1
-        while j >=0 and key < arr[j] : 
-                arr[j+1] = arr[j] 
+        while j >=0 and key < items[j] : 
+                items[j+1] = items[j] 
                 j -= 1
-        arr[j+1] = key 
+        items[j+1] = key
+
+        return items
